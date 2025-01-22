d634635e46bba9f094a6b9b731c4e5aa72b82738
sh <sh_single@163.com>
2025-01-22 14:23:45 +0800

    fix: 修改 ESLint 配置
    - 在 vite.config.ts 文件中添加 eslint-disable-next-line 注释
    - 解决 ESLint 对 no-undef 规则的警告

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // 使用 defineConfig 的类型定义
+// eslint-disable-next-line no-undef
 export default defineConfig(({ mode }) => {
   const base = mode === "production" ? "/todo-list-actions-deploy/" : "/";
 
